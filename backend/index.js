05d343f9f4450e5aa8acd3b68333573295bd867b
JuanGtz <jucasaguipn09@gmail.com>
2022-12-13 18:44:45 -0600

    FrontBase

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const dotenv = require('dotenv').config();
+const path = require('path');
 const app = express();
+
+app.set('views',path.join(__dirname,'../frontend/views'));
+app.engine('hbs',hbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs',
+}));
+app.set('view engine','hbs');
 
 app.use(express.urlencoded({extended:false}));
 
 app.use('/',require('./router/mainRouter'))
 app.use('/cliente',require('./router/usuarioRoutes'))
 app.use('/checkIn',require('./router/checkInRoutes'))
+
+app.use('/public',express.static(path.join(__dirname,'../frontend/public')));
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log('Server ON');
--- backend/router/mainRouter.js
+++ backend/router/mainRouter.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const {altaHabitacion,getHabitaciones} = require('../controller/habitacionController')
 const {getReservaciones,bajaReservacion} = require('../controller/reservacionController')
 
+router.get('/',(req,res)=>{
+    res.render('index')
+})
+
 router.post('/altaHab',altaHabitacion);
 
 router.get('/obtenHab',getHabitaciones);
